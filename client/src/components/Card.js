298143df175ead2fea88d78d7a70fd8089eb46c3
Tri <vutriminh@gmail.com>
2023-02-24 00:17:57 -0500

    card page now styled

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,6 +6,8 @@ import DisplayLists from './DisplayLists'
 import Random from './Random'
 import Search from './Search'
 
+import './Card.css'
+
 
 const Card = () => {
 
@@ -15,6 +17,7 @@ const Card = () => {
   const [cardName, setCardName] = useState()
   const [addedPrice, setAddedPrice] = useState(0)
   const [currentPrice, setCurrentPrice] = useState(0)
+  const [legalities, setLegalities] = useState([])
 
 
   let {card} = useParams()
@@ -23,11 +26,12 @@ const Card = () => {
 
     const data = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${card}`)
     const details = await data.json();
-    // console.log("cardInfo: ", details);
+    console.log("cardInfo: ", details);
     setCardInfo(details);
     setCardName(details.name);
     setAddedPrice(details.prices.usd);
     setCurrentPrice(details.prices.usd);
+    setLegalities(details.legalities)
 
   }
 
@@ -46,9 +50,9 @@ const Card = () => {
   useEffect(() => {
 
     cardDetail()
-
+    
   }, [])
-
+  
   return (
     <>
         Card
@@ -56,20 +60,115 @@ const Card = () => {
         <Search />
         <Random />
 
+        <AddCards cardName={cardName} addedPrice={addedPrice} currentPrice={currentPrice} />
         {
-          cardInfo === undefined
+          (cardInfo && legalities) === undefined
           ?
           // <h1>NO CARD IN DATABASE</h1>
           ''
           :
-          <div>
-            <h1>Card Name: {cardInfo.name}</h1>
-            <img src={cardInfo.image_uris.normal} />
-            <p>Cost: ${cardInfo.prices.usd} USD</p>
+          
+          <div className='contain'>
+            <div className='left'>
+              <div className='left-contain'>
+                <h1 style={{ textAlign : 'center' }}>{cardInfo.name}</h1>
+                <span>Mana Cost: {cardInfo.mana_cost}</span>
+                <span className='sec-span'>Artist: {cardInfo.artist}</span>
+                <br /><br />
+                <span>Type: {cardInfo.type_line}</span>
+                <span className='sec-span'>Rarity: {cardInfo.rarity}</span>
+                <p className='to-br'>{cardInfo.oracle_text}<br /><i>{cardInfo.flavor_text}</i></p>
+                <span>Current Cost: ${cardInfo.prices.usd} USD</span>
+                <span className='sec-span'>Release Date: {cardInfo.released_at}</span>
+              </div>
+              <br /><br />
+              {/* <tr>
+                <th></th>
+                <th></th>
+              </tr> */}
+              <table>
+                <thead>
+                  <tr>
+                    {/* <th></th> */}
+                    <th>Legalities</th>
+                    {/* <th></th> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Alchemy: {legalities.alchemy}</td>
+                  </tr>
+                  <tr>
+                    <td>Brawl: {legalities.brawl}</td>
+                  </tr>
+                  <tr>
+                    <td>Commander: {legalities.brawl}</td>
+                  </tr>
+                  <tr>
+                    <td>Duel: {legalities.duel}</td>
+                  </tr>
+                  <tr>
+                    <td>Explorer: {legalities.explorer}</td>
+                  </tr>
+                  <tr>
+                    <td>Future: {legalities.future}</td>
+                  </tr>
+                  <tr>
+                    <td>Gladiator: {legalities.gladiator}</td>
+                  </tr>
+                  <tr>
+                    <td>Historic: {legalities.historic}</td>
+                  </tr>
+                  <tr>
+                    <td>Historic Brawl: {legalities.historicbrawl}</td>
+                  </tr>
+                  <tr>
+                    <td>Legacy: {legalities.legacy}</td>
+                  </tr>
+                  <tr>
+                    <td>Modern: {legalities.modern}</td>
+                  </tr>
+                  <tr>
+                    <td>Old School: {legalities.oldschool}</td>
+                  </tr>
+                  <tr>
+                    <td>Pauper: {legalities.pauper}</td>
+                  </tr>
+                  <tr>
+                    <td>Pauper Commander: {legalities.paupercommander}</td>
+                  </tr>
+                  <tr>
+                    <td>Penny: {legalities.penny}</td>
+                  </tr>
+                  <tr>
+                    <td>Pioneer: {legalities.pioneer}</td>
+                  </tr>
+                  <tr>
+                    <td>PreDH: {legalities.predh}</td>
+                  </tr>
+                  <tr>
+                    <td>Premodern: {legalities.premodern}</td>
+                  </tr>
+                  <tr>
+                    <td>Standard: {legalities.standard}</td>
+                  </tr>
+                  <tr>
+                    <td>Vintage: {legalities.vintage}</td>
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
+
+            
+            <div className='right'>
+              <br />
+              <img src={cardInfo.image_uris.normal} />
+              <br />
+            </div>
           </div>
         }
 
-          <AddCards cardName={cardName} addedPrice={addedPrice} currentPrice={currentPrice} />
         {/* <form>
           <label>How many would you like to add?<input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} /></label>
           
